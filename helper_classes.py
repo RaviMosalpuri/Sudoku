d96f8890feffebcc41f30fef49e6c4eaa41f789f
Ravi Mosalpuri <ravinmosalpuri@gmail.com>
2025-01-13 21:56:02 +0000

    [Sudoku] Minor changes: Added more relevant comments

--- helper_classes.py
+++ helper_classes.py
@@ -58,9 +58,10 @@ class SudokuApp:
         self.elapsed_time = 0
         self.mistakes = 0
 
-        # Display timer and mistakes
+        # Display timer at the top, updated every second
         self.timer_label = tk.Label(self.root, text="Time: 0:00", font=("Arial", 14))
         self.timer_label.grid(row=0, column=0, columnspan=2, pady=10)
+        # Display the mistakes count, updated as mistakes made
         self.mistakes_label = tk.Label(self.root, text="Mistakes: 0/3", font=("Arial", 14))
         self.mistakes_label.grid(row=0, column=5, columnspan=4, pady=10)
 
@@ -175,8 +176,10 @@ class SudokuBoard:
                 # Create a frame for the box
                 frame = tk.Frame(self.root, highlightbackground="black", highlightthickness=1)
                 frame.grid(row=box_row * 3 + 1, column=box_col * 3, rowspan=3, columnspan=3, padx=1, pady=1)
+                # Loop over the small box
                 for row in range(3):
                     for col in range(3):
+                        # Get the r and c values as per the large sudoku
                         r, c = box_row * 3 + row, box_col * 3 + col
                         # Create a 'Entry' for user input
                         entry = tk.Entry(frame, width=2, font=("Arial", 18), justify="center")
@@ -198,18 +201,21 @@ class SudokuBoard:
         Function to check if the number (value) corresponding to the row and column is valid. It checks in the row, column and same box for the same number.
 
         Parameters:
-        sudoku : entry
-        row : row number
-        col : column number
+        r : int, row number
+        c : int, column number
+        val : int, value to check
         """
+        # Check for value in same column but other rows
         for row in range(9):
             if self.sudoku[row][c] == val:
                 return False
+        # Check for value in same row but other columns
         for col in range(9):
             if self.sudoku[r][col] == val:
                 return False
         r_start = (r // 3) * 3
         c_start = (c // 3) * 3
+        # Check for value in the small box
         for row in range(r_start, r_start + 3):
             for col in range(c_start, c_start + 3):
                 if self.sudoku[row][col] == val:
@@ -222,51 +228,80 @@ class SudokuBoard:
         Function to solve the sudoku puzzle.
         
         Parameters:
-        row : row number
-        col : column number
+        row : int, row number
+        col : int, column number
         """
+        # Check if the last value in the sudoku board is reached
         if row == 8 and col == 9:
+            # Return True
             return True
+        # Check if the last column is reached
         if col == 9:
+            # Go to next row
             row += 1
+            # Start from the first column
             col = 0
+        # Check if the current value is not zero
         if self.sudoku[row][col] != 0:
+            # Solve the next value in the row
             return self.__solve_sudoku(row, col + 1)
+        # Loop for the values
         for val in range(1, 10):
+            # Check if the value is safe to add
             if self.__is_number_valid(row, col, val):
+                # Add the value
                 self.sudoku[row][col] = val
+                # Check if the next value is safe to add
                 if self.__solve_sudoku(row, col + 1):
                     return True
+            # Back track, remove the value as this is not safe or valid
             self.sudoku[row][col] = 0
         return False
 
 
-    def __generate_random_sudoku(self, level=0):
+    def __generate_random_sudoku(self, difficulty=0):
         """
         Function to generate a random sudoku puzzle.
 
         Parameters:
-        level : int, level of difficulty
+        difficulty : int, level of difficulty
         """
+        # Initialise all the values with 0
         self.sudoku = [[0 for _ in range(9)] for _ in range(9)]
+        # Loop to fill the diagonal boxes in the sudoku
         for i in range(0, 9, 3):
+            # Get number from 1 to 10 in shuffled way
             nums = np.random.choice(np.arange(1, 10), 9, replace=False).tolist()
+            # Loop to iterate over the row and columns
             for row in range(3):
                 for col in range(3):
+                    # Pop the number from list and add it to the sudoku
                     self.sudoku[row + i][col + i] = nums.pop()
         
+        # Solve the sudoku
         self.__solve_sudoku()
+        # Deepcopy the solved sudoku
         self.sudoku_solved = deepcopy(self.sudoku)
         
-        if level == 0:
+        # Check for the level of difficulty
+        # 0 is for easy level
+        if difficulty == 0:
+            # Pick number of empty cells randomly between 32 and 36
             num_empty_cells = random.randint(32, 36)
-        elif level == 1:
+        # 1 is for medium level
+        elif difficulty == 1:
+            # Pick number of empty cells randomly between 37 and 41
             num_empty_cells = random.randint(37, 41)
+        # else for hard level
         else:
+            # Pick number of empty cells randomly between 42 and 46
             num_empty_cells = random.randint(42, 46)
         
+        # Loop over the number of empty cells
         for _ in range(num_empty_cells):
+            # Get random value for row and column from 0 to 8
             row, col = random.randint(0, 8), random.randint(0, 8)
+            # Remove the value from the sudoku
             self.sudoku[row][col] = 0
 
 
@@ -279,17 +314,27 @@ class SudokuBoard:
         row : row number
         col : column number
         """
+        # Check if the entry has a '0' or empty value
         if entry.get() == '0' or entry.get() == '':
+            # Change the background to white
             entry.config(bg='white')
             self.sudoku[row][col] = 0
+        # Check if the value is not correct by comparing with the solved sudoku
         elif int(entry.get()) != self.sudoku_solved[row][col]:
+            # Change the background to red
             entry.config(bg='red')
+            # Update the game mistakes
             self.app.update_mistakes()
+        # Else the value entered is correct
         else:
+            # Change the background to white
             entry.config(bg='white')
+            # Add the value to the sudoku
             self.sudoku[row][col] = self.sudoku_solved[row][col]
             
+            # Check if the complete sudoku is solved
             if self.check_if_solved():
+                # Call won game function
                 self.app.won_game()
 
 
@@ -297,10 +342,14 @@ class SudokuBoard:
         """
         Function to fill the grid with the solved puzzle.
         """
+        # Loop over the row and columns in sudoku
         for r in range(9):
             for c in range(9):
+                # Delete the entry value
                 self.entries[r][c].delete(0, tk.END)
+                # Insert the value from solved sudoku
                 self.entries[r][c].insert(0, str(self.sudoku_solved[r][c]))
+                # Disable the input
                 self.entries[r][c].config(state="disabled")
 
 
@@ -308,10 +357,14 @@ class SudokuBoard:
         """
         Function to clear all user inputs and reset the grid.
         """
+        # Loop over the row and columns in sudoku
         for r in range(9):
             for c in range(9):
+                # Check if the entry state is not disabled, that is entry was empty at the start
                 if self.entries[r][c].cget('state') != 'disabled':
+                    # Delete the entered value
                     self.entries[r][c].delete(0, tk.END)
+                    # Change the background to white
                     self.entries[r][c].config(bg="white", state="normal")
 
 
@@ -319,11 +372,18 @@ class SudokuBoard:
         """
         Function to disable all inputs on editable cells.
         """
+        # Loop over the entries
         for row in self.entries:
             for entry in row:
+                # Check if the entry state is not disabled, that is entry was empty at the start
                 if entry["state"] != "disabled":
+                    # Disable the input
                     entry.config(state="disabled")
 
     
     def check_if_solved(self):
+        """
+        Function to check if sudoku is solved or not.
+        """
+        # Check if the sudoku is same as solved sudoku
         return self.sudoku == self.sudoku_solved
